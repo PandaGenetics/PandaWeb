2b9ea65cb82f12a0011f8d44a8ca2fd839848310
xuweixw <oncwnuHygo0EWnXs-U8g-sgXgnP8@git.weixin.qq.com>
2022-02-11 16:54:14 +0800

    intergrate three pages

--- main.go
+++ main.go
@@ -12,8 +12,22 @@ func main() {
 
 	router.Static("/static", "static/")
 
+	router.Static("/img", "images")
+
 	router.GET("", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "home_page.html", gin.H{})
+	})
+
+	router.GET("/genebrowser", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "GeneBrowser.html", gin.H{})
+	})
+
+	router.GET("/pedigree", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "pedigree.html", gin.H{})
 	})
 
 	router.Run(":8080")
