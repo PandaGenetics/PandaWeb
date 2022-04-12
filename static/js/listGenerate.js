58b5d02a968d426e9010de9d213fef0fba4bc8ba
xuweixw <oncwnuHygo0EWnXs-U8g-sgXgnP8@git.weixin.qq.com>
2022-04-12 21:11:36 +0800

    fix ajax by window.Fetch API

--- static/js/listGenerate.js
+++ static/js/listGenerate.js
@@ -1,37 +1,43 @@
-function haha(){
-    var List = (function (){
-        let List = null;
-        $.ajax({
-            "url": "gene/data.json",
-            "type": "GET",
-            "dataType": "json",
-            "async": false,
-            "sucess":function(data){
-                List = data;
-                console.log("data");
-            }
-        })
-        return List;
-    })();
-}
+// function haha(){
+//     var List = (function (){
+//         let List = null;
+//         $.ajax({
+//             "url": "gene/data.json",
+//             "type": "GET",
+//             "dataType": "json",
+//             "async": false,
+//             "sucess":function(data){
+//                 List = data;
+//                 console.log("data");
+//             }
+//         })
+//         return List;
+//     })();
+// }
 
 function setDiv(){
-    haha();
+    let List = [];
+    fetch("gene/data.json") //Asynchronous function
+        .then( res => res.json() )
+        .then( result => appendHTML(result) ); // callback
+}
+
+function appendHTML(List) {
     for (let j = 0; j < List.length; j++) {
         let svp = List[j];
         //generate html format and transmit to geneBrowser@v2.html
         let html1 =
-            '<tr>'
+            '<tr>' +
                 '<td>' + svp.url + '</td>' +
                 '<td><label>' +
-                    '<input type="checkbox" className="track-select isotype-item sample-track" value="' + svp.url + '">VCF' +
+                '<input type="checkbox" className="track-select isotype-item sample-track" value="' + svp.url + '">VCF' +
                 '</label></td>' +
                 '<td><label>' +
-                    '<input type="checkbox" className="track-select isotype-item sample-track" value="' + svp.url + '">BAM' +
+                '<input type="checkbox" className="track-select isotype-item sample-track" value="' + svp.url + '">BAM' +
                 '</label></td>' +
-            '</tr>'
+            '</tr>';
 
-         $(".content").append(html1)
+        $(".content").append(html1)
     }
 }
 
