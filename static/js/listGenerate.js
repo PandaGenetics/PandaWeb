f50de4dadb370095913581893fff638b0178b32b
C12-H22 <kevinkanghy@gmail.com>
2022-04-12 21:54:11 +0800

    list not finish

--- static/js/listGenerate.js
+++ static/js/listGenerate.js
@@ -1,37 +1,57 @@
-function haha(){
-    var List = (function (){
-        let List = null;
-        $.ajax({
-            "url": "gene/data.json",
-            "type": "GET",
-            "dataType": "json",
-            "async": false,
-            "sucess":function(data){
-                List = data;
-                console.log("data");
+window.onload = function (){
+    var url = "gene/data.json"
+    var request = new XMLHttpRequest();
+    request.open("get", url);
+    request.send(null;)
+
+    request.onload = function () {
+        if (request.status == 200){
+            var targethtml = "";
+            var json = JSON.parse(request.responseText);
+            for(var i=0;i<json.length;i++){
+                console.log(json[i],url);
+                targethtml +=  "<tr><td><input type='checkbox'/>"+ json[i].url +"</td></tr>"
             }
-        })
-        return List;
-    })();
-}
-
-function setDiv(){
-    haha();
-    for (let j = 0; j < List.length; j++) {
-        let svp = List[j];
-        //generate html format and transmit to geneBrowser@v2.html
-        let html1 =
-            '<tr>'
-                '<td>' + svp.url + '</td>' +
-                '<td><label>' +
-                    '<input type="checkbox" className="track-select isotype-item sample-track" value="' + svp.url + '">VCF' +
-                '</label></td>' +
-                '<td><label>' +
-                    '<input type="checkbox" className="track-select isotype-item sample-track" value="' + svp.url + '">BAM' +
-                '</label></td>' +
-            '</tr>'
-
-         $(".content").append(html1)
+            console.log(json);
+        }
+        document.getElementById("genetic").innerHTML = targethtml;
     }
 }
 
+
+
+
+// function setDiv(){
+//     console.log("haha");
+//     var List = (function (){
+//         let List = null;
+//         $.ajax({
+//             "url": "gene/data.json",
+//             "type": "GET",
+//             "dataType": "json",
+//             "async": false,
+//             "sucess":function(data){
+//                 List = data;
+//                 console.log("data");
+//             }
+//         })
+//         return List;
+//     })();
+//     for (let j = 0; j < List.length; j++) {
+//         let svp = List[j];
+//         //generate html format and transmit to geneBrowser@v2.html
+//         let html1 =
+//             '<tr>'+
+//                 '<td>' + svp.url + '</td>' +
+//                 '<td><label>' +
+//                     '<input type="checkbox" className="track-select isotype-item sample-track" value="' + svp.url + '">VCF' +
+//                 '</label></td>' +
+//                 '<td><label>' +
+//                     '<input type="checkbox" className="track-select isotype-item sample-track" value="' + svp.url + '">BAM' +
+//                 '</label></td>' +
+//             '</tr>'
+//          $("#genetic").append(html1)
+//     }
+//
+// }
+//
