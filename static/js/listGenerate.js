f3a4f80c692d4dc8ccf750e6b16ea47e6e8533c8
C12-H22 <kevinkanghy@gmail.com>
2022-04-13 14:33:39 +0800

    listGenerate.js fix

--- static/js/listGenerate.js
+++ static/js/listGenerate.js
@@ -22,27 +22,7 @@ window.onload = function (){
                     "</label></td>"+
                     "</tr>"
             }
-            console.log(json);
         }
         document.getElementById("genetic").innerHTML = targetHTML;
-    }
-}
-
-function appendHTML(List) {
-    for (let j = 0; j < List.length; j++) {
-        let svp = List[j];
-        //generate html format and transmit to geneBrowser@v2.html
-        let html1 =
-            '<tr>' +
-                '<td>' + svp.url + '</td>' +
-                '<td><label>' +
-                '<input type="checkbox" className="track-select isotype-item sample-track" value="' + svp.url + '">VCF' +
-                '</label></td>' +
-                '<td><label>' +
-                '<input type="checkbox" className="track-select isotype-item sample-track" value="' + svp.url + '">BAM' +
-                '</label></td>' +
-            '</tr>';
-
-        $(".content").append(html1)
     }
 }
